cc2c66d4e8ffd533477776e125a2c8557f4015b7
Seonghun <77530278+kawai10@users.noreply.github.com>
2022-03-09 14:28:33 +0900

    Add product model

--- src/company/models.js
+++ src/company/models.js
@@ -1,83 +1,88 @@
-import SQ from "sequelize"
-import {sequelize} from "../../connection/dbConnection.js"
-import {Customer} from "../customers/models.js"
+import SQ from "sequelize";
+import { sequelize } from "../../connection/dbConnection.js";
+import { Customer } from "../customers/models.js";
 const DataTypes = SQ.DataTypes;
 
 // 테이블 정의
-const Company = sequelize.define('company', {
-    id : {
-        type: DataTypes.INTEGER,
-        autoIncrement : true,
-        primaryKey : true,
-        allowNull : false,
+const Company = sequelize.define("company", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING(50),
+    unique: true,
+    allowNull: false,
+    validate: {
+      isEmail: true,
     },
-    email : {
-        type: DataTypes.STRING(50),
-        unique: true,
-        allowNull : false,
-        validate :{
-            isEmail : true
-        }
-    },
-    password : {
-        type: DataTypes.STRING(50),
-        allowNull : false,
-    },
-    key : {
-        type: DataTypes.STRING(100),
-        allowNull : true,
-        unique : true,
-    },
-    name : {
-        type : DataTypes.STRING(20),
-        allowNull : false
-    },
-    createdAt : {
-        type : DataTypes.DATEONLY
-    },
-    updatedAt : {
-        type : DataTypes.DATEONLY
-    }
-})
-Company.hasMany(Customer, {as : "customer"})
+  },
+  password: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+  key: {
+    type: DataTypes.STRING(100),
+    allowNull: true,
+    unique: true,
+  },
+  name: {
+    type: DataTypes.STRING(20),
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATEONLY,
+  },
+  updatedAt: {
+    type: DataTypes.DATEONLY,
+  },
+});
+Company.hasMany(Customer, { as: "customer" });
 
-async function createCompanyObject(id, email, password, key, name){
-    // TODO : change variable name
-    const data = await Company.create({
-        id,
-        email,
-        password,
-        key,
-        name
-    });
-    return getCompanyObject(email, password);
+async function createCompanyObject(id, email, password, key, name) {
+  // TODO : change variable name
+  const data = await Company.create({
+    id,
+    email,
+    password,
+    key,
+    name,
+  });
+  return getCompanyObject(email, password);
 }
 
-function getCompanyObject(email, password){
-    return Company.findOne({
-        attributes : { exclude : ['password']},
-        where : {
-            email,
-            password
-        }
-    })
+function getCompanyObject(email, password) {
+  return Company.findOne({
+    attributes: { exclude: ["password"] },
+    where: {
+      email,
+      password,
+    },
+  });
 }
 
-function deleteCompanyObject(email, password){
-    return Company.destroy({
-        where : {
-            email,
-            password
-        }
-    })
+function deleteCompanyObject(email, password) {
+  return Company.destroy({
+    where: {
+      email,
+      password,
+    },
+  });
 }
 
-function validateApiKey(key){
-    return Company.findOne({
-        where : {
-            key
-        }
-    })
+function validateApiKey(key) {
+  return Company.findOne({
+    where: {
+      key,
+    },
+  });
 }
 
-export {createCompanyObject, getCompanyObject, validateApiKey, deleteCompanyObject}
+export {
+  createCompanyObject,
+  getCompanyObject,
+  validateApiKey,
+  deleteCompanyObject,
+};
--- src/product/models.js
+++ src/product/models.js
@@ -1,3 +1,25 @@
-import SQ from "sequelize"
-import {sequelize} from "Atripe/connection/dbConnection.js"
+import SQ from "sequelize";
+import { sequelize } from "../../connection/dbConnection.js";
 const DataTypes = SQ.DataTypes;
+
+const Poduct = sequelize.define("product", {
+  id: {
+    type: DataTypes.STRING(50),
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING(15),
+    allowNull: false,
+  },
+  active: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true,
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATEONLY,
+  },
+  updatedAt: {
+    type: DataTypes.DATEONLY,
+  },
+});
